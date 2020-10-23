6c226e6f3b71ed6cd065e57563bbc8110c0ca849
Eileen Rhodes <etrhodes619@gmail.com>
2020-10-23 11:57:37 -0600

    error updates

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 function getDogImage(breed) {
     fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        .then(response => response.json())
-        .then(responseJson => displayImages(responseJson))
-        .catch(error => alert('Something went wrong. Try again later'));
+        .then(response => {
+            if (!response.ok) {
+                console.error(response.message)
+            } else {
+            response.json()
+        }
+    })
+        .then(responseJson =>
+            displayImages(responseJson)
+        )
+        .catch(error => alert('Something went wrong. Try again later')
+        );
 }
 
 function onSubmit() {
     $('form').on('submit', event => {
         event.preventDefault();
         let breed = $('input[type="text"]').val();  
-        console.log(breed);
         getDogImage(breed);  
     });
 }
 
 function displayImages(responseJson) {
-    console.log(responseJson);
-    $('#target').append(`<img src="${responseJson.message[0]}" class="results-img">`);
+    $('#target').replaceWith(`<img src="${responseJson.message[0]}" class="results-img">`);
     $('.results').removeClass('hidden');
 };
-
-
 
 function handler() {
     onSubmit();
